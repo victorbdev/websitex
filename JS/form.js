f78a8a68c2bb687ba85b3c75e748d46f98259880
DVictorB <david.balboa@tecsup.edu.pe>
2022-06-05 15:41:39 -0500

    Subiendo proyecto web

---
+++ JS/form.js
@@ -0,0 +1,8 @@
+var getData = function(){
+    var fname = document.getElementById("name").value;
+    var lname = document.getElementById("lastname").value;
+    var email = document.getElementById("correo").value;
+    var phone = document.getElementById("mensaje").value;
+    var message = document.getElementById("lastname").value;
+    console.log(fname+" "+lname+" "+email+" "+phone+" "+message)
+}
